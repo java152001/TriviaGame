2f71f1d75b4017fc4e552af042d3470e270043c1
Anthony Demott <java152001@gmail.com>
2018-08-25 14:50:15 -0700

    Logic is mostly done

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,23 +1,58 @@
 $(document).ready(function() {
 
-var firstQuestion = ["What is the name of the city Ash is from?","Phoenix", "Pallet", "Saffron", "Tokyo"];
-var secondQuestion = ["Which is the first pokemon in the national pokedex?","Pikachu", "Dragonite", "Bulbasaur", "Ponyta"];
-var thirdQuestion = ["How many evolutions does eevee have?","3", "5", "8", "10"];
-var fourthQuestion = ["Which of the following is a generation 2 starter?", "Bidoof", "Totodile", "Hoothoot", "Lugia"];
-var fifthQuestion = ["Which pokeball has the highest capture rate?", "Masterball", "Pokeball", "Heavy ball", "Quick ball"];
-var sixthQuestion = ["Regular repels will last how long?", "50 steps", "100 steps", "150 steps", "200 steps"];
-var seventhQuestion = ["Which of the following WON'T evolve from a Moon stone?", "Clefairy", "Nidorino", "Snubble", "Jigglypuff"];
-var eighthQuestion = ["Which of the following is NOT super effective vs dragon types?", "Ice Attacks", "Dark Attacks", "Fairy Attacks", "Dragon Attacks"];
-var ninthQuestion = ["What do you have to get to wake up Snorlax?", "Alarm Clock", "Poke Flute", "Smelling Salts", "Bucket of Water"];
-var tenthQuestion = ["Which pokemon evolves when you trade it?", "Haunter", "Hitmonchan", "Wooper", "Mew"];
+var questions = [
+        {question: "not used"},
+        {
+        question: "What is the name of the city Ash is from?",
+        options: ["Phoenix", "Pallet", "Saffron", "Tokyo"],
+        correct: "Pallet"},
+        {
+        question: "Which is the first pokemon in the national pokedex?",
+        options: ["Pikachu", "Dragonite", "Bulbasaur", "Ponyta"],
+        correct: "Bulbasaur"},
+        {
+        question: "How many evolutions does eevee have?",
+        options: ["3", "5", "8", "10"],
+        correct: "8"},
+        {
+        question: "Which of the following is a generation 2 starter?",
+        options: ["Bidoof", "Totodile", "Hoothoot", "Lugia"],
+        correct: "Totodile"},
+        {
+        question: "Which pokeball has the highest capture rate?",
+        options: ["Masterball", "Pokeball", "Heavy ball", "Quick ball"],
+        correct: "Masterball"},
+        {
+        question: "Regular repels will last how long?",
+        options: ["50 steps", "100 steps", "150 steps", "200 steps"],
+        correct: "100 steps"},
+        {
+        question: "Which of the following WON'T evolve from a Moon stone?",
+        options: ["Clefairy", "Nidorino", "Snubble", "Jigglypuff"],
+        correct: "Snubble"},
+        {
+        question: "Which of the following is NOT super effective vs dragon types?",
+        options: ["Ice Attacks", "Dark Attacks", "Fairy Attacks", "Dragon Attacks"],
+        correct: "Dark Attacks"},
+        {
+        question: "What do you have to get to wake up Snorlax?",
+        options: ["Alarm Clock", "Poke Flute", "Smelling Salts", "Bucket of Water"],
+        correct: "Poke Flute"},
+        {
+        question: "Which pokemon evolves when you trade it?",
+        options: ["Haunter", "Hitmonchan", "Wooper", "Mew"],
+        correct: "Haunter"}]
 
 var timeout = false;
+
+var questionTrack = 1
 
 var correct = 0;
 var wrong = 0;
 
 var countDown = 11;
 var intervalId;
+var nextQuestionInterval;
 
 $("#start").on("click", function() {
 
@@ -38,256 +73,44 @@ $("#start").on("click", function() {
 
     $("#container").append(answer_space);
 
-    questionOne();
-
-// function generateQuestion(questionNumber, ) {
-//     $("#question-space").text(info[0]);
-
-//     clearInterval(intervalId);
-//     intervalId = setInterval(decrement, 1000);
-
-//     renderAnswers(info);
-
-//     $(".questions").on("click", function() {
-//         if ($(this).attr("data-value") === "1") {
-//             alert("Correct");
-//             questionTwo();
-//         }
-//         else {
-//             alert("Wrongzo");
-//             questionTwo();
-//         }
-//     });
-// }
+    generateQuestion(questionTrack, 2);
 
 });
 
-function questionOne() {
-    $("#question-space").text(firstQuestion[0]);
+    
+
+function generateQuestion(questionNumber) {
+    $("#question-space").text(questions[questionNumber].question);
 
     clearInterval(intervalId);
     intervalId = setInterval(decrement, 1000);
 
-    renderAnswers(firstQuestion);
+    renderAnswers(questionNumber);
 
-        $(".questions").on("click", function() {
-            if ($(this).attr("data-value") === "2") {
-                alert("Correct");
-                correct++;
-                questionTwo();
-            }
-            else {
-                alert("Wrongzo");
-                wrong++;
-                questionTwo();
-            }
-        });
-}
-
-function questionTwo() {
-
-    $("#question-space").text(secondQuestion[0]);
-
-    countDown = 11;
-    clearInterval(intervalId);
-    intervalId = setInterval(decrement, 1000);
-
-    renderAnswers(secondQuestion);
+    var correctAnswer = questions[questionNumber].correct;
 
     $(".questions").on("click", function() {
-        if ($(this).attr("data-value") === "3") {
+        if ($(this).text() === correctAnswer) {
+            console.log($(this).attr("data-value"));
+            console.log(correctAnswer);
             alert("Correct");
-            correct++;
-            questionThree();
+            questionTrack++
+            clearInterval(intervalId);
+            countDown = 11;
+            setTimeout(function() {
+                generateQuestion(questionTrack);
+              }, 3000);
         }
         else {
+            console.log($(this).attr("data-value"));
+            console.log(correctAnswer);
             alert("Wrongzo");
-            wrong++;
-            questionThree();
-        }
-    });
-}
-
-function questionThree() {
-    $("#question-space").text(thirdQuestion[0]);
-
-    countDown = 11;
-    clearInterval(intervalId);
-    intervalId = setInterval(decrement, 1000);
-
-    renderAnswers(thirdQuestion);
-
-    $(".questions").on("click", function() {
-        if ($(this).attr("data-value") === "3") {
-            alert("Correct");
-            correct++;
-            questionFour();
-        }
-        else {
-            alert("Wrongzo");
-            wrong++;
-            questionFour();
-        }
-    });
-}
-
-function questionFour() {
-    $("#question-space").text(fourthQuestion[0]);
-
-    countDown = 11;
-    clearInterval(intervalId);
-    intervalId = setInterval(decrement, 1000);
-
-    renderAnswers(fourthQuestion);
-
-    $(".questions").on("click", function() {
-        if ($(this).attr("data-value") === "2") {
-            alert("Correct");
-            correct++;
-            questionFive();
-        }
-        else {
-            alert("Wrongzo");
-            wrong++;
-            questionFive();
-        }
-    });
-}
-
-function questionFive() {
-    $("#question-space").text(fifthQuestion[0]);
-
-    countDown = 11;
-    clearInterval(intervalId);
-    intervalId = setInterval(decrement, 1000);
-
-    renderAnswers(fifthQuestion);
-
-    $(".questions").on("click", function() {
-        if ($(this).attr("data-value") === "1") {
-            alert("Correct");
-            correct++;
-            questionSix();
-        }
-        else {
-            alert("Wrongzo");
-            wrong++;
-            questionSix();
-        }
-    });
-}
-
-function questionSix() {
-    $("#question-space").text(sixthQuestion[0]);
-
-    countDown = 11;
-    clearInterval(intervalId);
-    intervalId = setInterval(decrement, 1000);
-
-    renderAnswers(sixthQuestion);
-
-    $(".questions").on("click", function() {
-        if ($(this).attr("data-value") === "2") {
-            alert("Correct");
-            correct++;
-            questionSeven();
-        }
-        else {
-            alert("Wrongzo");
-            wrong++;
-            questionSeven();
-        }
-    });
-}
-
-function questionSeven() {
-    $("#question-space").text(seventhQuestion[0]);
-
-    countDown = 11;
-    clearInterval(intervalId);
-    intervalId = setInterval(decrement, 1000);
-
-    renderAnswers(seventhQuestion);
-
-    $(".questions").on("click", function() {
-        if ($(this).attr("data-value") === "3") {
-            alert("Correct");
-            correct++;
-            questionEight();
-        }
-        else {
-            alert("Wrongzo");
-            wrong++;
-            questionEight();
-        }
-    });
-}
-
-function questionEight() {
-    $("#question-space").text(eighthQuestion[0]);
-
-    countDown = 11;
-    clearInterval(intervalId);
-    intervalId = setInterval(decrement, 1000);
-
-    renderAnswers(eighthQuestion);
-
-    $(".questions").on("click", function() {
-        if ($(this).attr("data-value") === "2") {
-            alert("Correct");
-            correct++;
-            questionNine();
-        }
-        else {
-            alert("Wrongzo");
-            wrong++;
-            questionNine();
-        }
-    });
-}
-
-function questionNine() {
-    $("#question-space").text(ninthQuestion[0]);
-
-    countDown = 11;
-    clearInterval(intervalId);
-    intervalId = setInterval(decrement, 1000);
-
-    renderAnswers(ninthQuestion);
-
-    $(".questions").on("click", function() {
-        if ($(this).attr("data-value") === "2") {
-            alert("Correct");
-            correct++;
-            questionTen();
-        }
-        else {
-            alert("Wrongzo");
-            wrong++;
-            questionTen();
-        }
-    });
-}
-
-function questionTen() {
-    $("#question-space").text(tenthQuestion[0]);
-
-    countDown = 11;
-    clearInterval(intervalId);
-    intervalId = setInterval(decrement, 1000);
-
-    renderAnswers(tenthQuestion);
-
-    $(".questions").on("click", function() {
-        if ($(this).attr("data-value") === "1") {
-            alert("Correct");
-            correct++;
-            scoreSummary();
-        }
-        else {
-            alert("Wrongzo");
-            wrong++;
-            scoreSummary();
+            questionTrack++;
+            clearInterval(intervalId);
+            countDown = 11;
+            setTimeout(function() {
+                generateQuestion(questionTrack);
+              }, 3000);
         }
     });
 }
@@ -303,25 +126,37 @@ function scoreSummary() {
 
 
 // Passes in the array for the question we're on and generates the answer options.
-function renderAnswers(question) {
+function renderAnswers(questionNumber) {
     $("#answer-space").empty();
 
-    for (i = 1; i < question.length; i++) {
+    for (i = 0; i < questions[questionNumber].options.length; i++) {
         var questionBox = $("<div>");
         questionBox.attr("id","question-box");
         questionBox.attr("data-value", [i]);
         questionBox.attr("class","questions");
-        questionBox.text(question[i]);
+        questionBox.text(questions[questionNumber].options[i]);
         $("#answer-space").append(questionBox);
     }
 }
 
-
 function decrement() {
+
     countDown--;
     $("#timer-space").html("<h2>00:" + countDown + "</h2>");
-    if (countDown === -1) {
-        timeout = true;
+    if (countDown <= 0) {
+        // timeout = true;
+        // get right answer
+        // show it
+
+        rightAnswer = questions[questionTrack].correct;
+        alert("Time Up the correct answer was " + rightAnswer);
+        clearInterval(intervalId);
+
+        setTimeout(function() {
+            generateQuestion(questionTrack);
+          }, 3000);
+
+        countDown = 11;
     }
 }
 })
